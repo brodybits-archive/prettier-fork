6f2844c8dbfcd4ef35e7db5d63771656d4ecaaa3
Christopher J. Brody <chris.brody@gmail.com>
2019-01-13 15:54:00 -0500

    XXX TODO test trailing comma absent vs present in o*.json

--- tests/align-object-properties/json/jsfmt.spec.js
+++ tests/align-object-properties/json/jsfmt.spec.js
@@ -1,4 +1,5 @@
 run_spec(__dirname, ["json"]);
+// XXX TODO test trailing comma absent vs present in o*.json
 run_spec(__dirname, ["json"], { trailingComma: "all" });
 run_spec(__dirname, ["json5"]);
 run_spec(__dirname, ["json5"], { trailingComma: "all" });
