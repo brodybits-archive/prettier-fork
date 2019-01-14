b635037344a4ab032d8d1975be46ff40ed8b1cdc
Christopher J. Brody <chris.brody@gmail.com>
2019-01-13 20:22:55 -0500

    lint fixes in tests/align-object-properties

--- tests/align-object-properties/json/jsfmt.spec.js
+++ tests/align-object-properties/json/jsfmt.spec.js
@@ -1,6 +1,12 @@
-run_spec(__dirname, ["json"], {alignObjectProperties: true});
+run_spec(__dirname, ["json"], { alignObjectProperties: true });
 // XXX TODO test trailing comma absent vs present in o*.json
-run_spec(__dirname, ["json"], { alignObjectProperties: true, trailingComma: "all" });
-run_spec(__dirname, ["json5"], {alignObjectProperties: true});
-run_spec(__dirname, ["json5"], { alignObjectProperties: true, trailingComma: "all" });
-run_spec(__dirname, ["json-stringify"], {alignObjectProperties: true});
+run_spec(__dirname, ["json"], {
+  alignObjectProperties: true,
+  trailingComma: "all"
+});
+run_spec(__dirname, ["json5"], { alignObjectProperties: true });
+run_spec(__dirname, ["json5"], {
+  alignObjectProperties: true,
+  trailingComma: "all"
+});
+run_spec(__dirname, ["json-stringify"], { alignObjectProperties: true });
