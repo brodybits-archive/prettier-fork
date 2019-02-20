2e543ce6ef386179a4fa89959a834b63bada03cc
aMarCruz <amarcruzbox-git@yahoo.com>
2019-02-20 10:17:12 -0600

    fix alignTernaryLines behavior

--- tests/standard/jsfmt.spec.js
+++ tests/standard/jsfmt.spec.js
@@ -1,4 +1,6 @@
 run_spec(__dirname, ["babel", "flow", "typescript"], {
+  arrowParens: "always",
+  endOfLine: "lf",
   yieldStarSpacing: true,
   generatorStarSpacing: true,
   spaceBeforeFunctionParen: true,
